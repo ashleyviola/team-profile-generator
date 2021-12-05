a5978bd918df443331f09fda2495cb6e5f9f0f40
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-12-05 15:48:18 -0500

    built and tested employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,17 +2,28 @@ const Employee = require('../lib/Employee');
 
 // creates employee object 
 test('creates employee object', () => {
-    const employee = new Employee ('Ashley', '1','test@test.com');
+    const employee = new Employee ('Ashley', 1,'test@test.com');
 
-    expect(employee.name).toBe('Ashley');
-    expect(employee.id).toEqual('1');
-    expect(employee.email).toBe('test@test.com');
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 })
 
-// gets players name 
+// gets employee name 
+test('gets employee name', () => {
+    const employee = new Employee ('Ashley', 1,'test@test.com');
 
+    expect(employee.getName()).toEqual(expect.any(String));
+})
+// gets employee id 
+test('gets employee id', () => {
+    const employee = new Employee('Ashley', 1, 'test@test.com');
 
-// gets players id 
+    expect(employee.getId()).toEqual(expect.any(Number));
+})
+// get employee email 
+test('gets employee email', () => {
+    const employee = new Employee('Ashley', 1, 'test@test.com');
 
-
-// get players email 
+    expect(employee.getEmail()).toEqual(expect.any(String));
+})
